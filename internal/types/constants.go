2e24dc01c5b98bd9ad9098be63755cf351f4167b
EduardMikhrin <emihrin50@gmail.com>
2025-11-18 23:39:06 +0200

    initial

---
+++ internal/types/constants.go
@@ -0,0 +1,5 @@
+package types
+
+// DefaultUserPhoto is the default photo URL for users
+// Source: https://www.flaticon.com/free-icon/user_709699
+const DefaultUserPhoto = "https://cdn-icons-png.flaticon.com/512/709/709699.png"
